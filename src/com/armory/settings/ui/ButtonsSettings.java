12ad3923fb1026b04636e3364b129768b3f5330f
michael.s.corigliano@gmail.com <michael.s.corigliano@gmail.com>
2018-03-26 20:12:05 -0400

    Volume key cursor control (2/2)
    Change-Id: I08d9a97e51541ce12ca24b9912424258628f7806

--- src/com/armory/settings/ui/ButtonsSettings.java
+++ src/com/armory/settings/ui/ButtonsSettings.java
@@ -17,7 +17,10 @@
 package com.armory.settings.ui;
 
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 
@@ -25,16 +28,33 @@ import com.android.settings.R;
 
 public class ButtonsSettings extends InvictrixSettingsFragment implements OnPreferenceChangeListener {
 
+    private static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+    private ListPreference mCursorControl;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         title = getResources().getString(R.string.buttons_settings_title);
         addPreferencesFromResource(R.xml.settings_buttons);
+
+        int cursorVal = Settings.System.getIntForUser(getContentResolver(),
+                VOLUME_KEY_CURSOR_CONTROL, 0, UserHandle.USER_CURRENT);
+        mCursorControl = (ListPreference) findPreference(VOLUME_KEY_CURSOR_CONTROL);
+        mCursorControl.setValue(String.valueOf(cursorVal));
+        mCursorControl.setOnPreferenceChangeListener(this);
+
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return true;
+        if (preference == mCursorControl) {
+            int val = (Integer) newValue;
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.VOLUME_KEY_CURSOR_CONTROL, val, UserHandle.USER_CURRENT);
+            return true;
+        }
+        return false;
     }
 
 }
